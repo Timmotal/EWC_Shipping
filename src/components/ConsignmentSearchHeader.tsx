384e2b584dd636eacefcb6c635cfba99674520ae
unknown <timmortal44@yahoo.com>
2024-07-23 13:33:46 +0100

    replace consignment search header with header nav component

--- src/components/ConsignmentSearchHeader.tsx
+++ src/components/ConsignmentSearchHeader.tsx
@@ -1,13 +1,19 @@
 // import React from "react";
 
 import { NavLink } from "react-router-dom";
+import HeaderNav from "./HeaderNav";
 
 const ConsignmentSearchHeader = () => {
   return (
     <div
       className={`self-stretch rounded-lg  box-border flex flex-col items-start justify-start pt-8 px-8 gap-5 mb-5 max-w-full text-left text-[#0B0101] `}
     >
-      <div className="flex flex-row items-start justify-start">
+
+<HeaderNav
+      pageLinkUrl="/"
+      pageLinkText="Back to Dashboard" 
+    />
+      {/* <div className="flex flex-row items-start justify-start">
         <div className="rounded-md flex flex-row items-center justify-center py-[0.625rem] px-[1.437rem] gap-[0.5rem] border-[1px] border-solid border-[#0354A6]">
           <div className="flex flex-row items-center justify-center">
             {" "}
@@ -26,7 +32,7 @@ const ConsignmentSearchHeader = () => {
           </NavLink>
          
         </div>
-      </div>
+      </div> */}
 
       <div className="self-stretch flex flex-col items-start justify-start gap-[1.25rem] top-[0] z-[99] sticky max-w-full">
         <div className="self-stretch flex flex-col items-start justify-start">
